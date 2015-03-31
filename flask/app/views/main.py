1ad39583013431ad65ed7416739648be0b29ed9b
Evan <evan.locke@gmail.com>
2015-03-31 02:42:20 -0600

    cleaning up the views

--- flask/app/views/main.py
+++ flask/app/views/main.py
@@ -7,7 +7,4 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-	# artists = Artist.objects[40:50]
-	# for i in artists:
-	# 	print i    
-	return render_template('main/main.html')
+	return render_template('main.html')
