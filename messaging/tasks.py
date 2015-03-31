a6d6fed93c853119b0acad80216026f2f6cb12f0
Evan <evan.locke@gmail.com>
2015-03-31 02:40:22 -0600

    making it easier to add data

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -5,13 +5,24 @@ import time
 import processMetadata_v3
 from internetarchive import search_items
 
-search = search_items('mediatype:etree AND creator:"Blue Turtle Seduction"')
+def queueSearch(search_string):
+	redis_conn = Redis('redis')
+	q = Queue(connection=redis_conn)
+	search = search_items(search_string)
+
+	for result in search:
+		print result['identifier']
+		job = q.enqueue(processMetadata_v3.main, result['identifier'])
 
 
-redis_conn = Redis('redis')
-q = Queue(connection=redis_conn)
 
-for result in search:
-	print result['identifier']
-	job = q.enqueue(processMetadata_v3.main, result['identifier'])
-	
+
+# Small Samples
+queueSearch('mediatype:etree AND (creator:Spafford OR creator:"Perpetual Groove" OR creator:Zoogma OR creator:"Mountain Standard Time")')
+
+
+
+# Full Artist Load
+# queueSearch('mediatype:collection AND collection:etree')
+# Full Show Load
+# queueSearch('mediatype:etree')
